d5f3e093c4eb4fcdefa3f4bfcfcd7013367d4b9f
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2023-07-21 17:49:52 +0600

    chore: add design home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,21 @@
+import { Button, Card, Input } from '@/components/shared';
+
 const HomePage = () => {
   return (
-    <div>
-      <h3 className="text-3xl">Home Page</h3>
+    <div className="mx-auto max-w-[1000px] space-y-5 px-2 py-4">
+      <Card>
+        <Card.Header>
+          <Card.Title>Short Your Url</Card.Title>
+        </Card.Header>
+        <Card.Body>
+          <Input type="text" placeholder="Enter The Url Here" />
+        </Card.Body>
+        <Card.Footer className="flex justify-center">
+          <Button color="primary" variant="solid" size="md" className="whitespace-nowrap">
+            Shorten Url
+          </Button>
+        </Card.Footer>
+      </Card>
     </div>
   );
 };
